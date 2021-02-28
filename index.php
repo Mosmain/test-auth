4d3ec750726fca4f64884d223cb2a633405daed8
Mosmain <mosmain@yandex.ru>
2021-02-28 22:54:40 +0400

    *

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
-<?php 
+<?php
 header('Content-type: text/html; charset=utf-8');
-session_start(); 
 require_once 'connect.php';
+session_start();
 
 // вывод всех пользователей
 $sql = "SELECT * FROM users";
@@ -12,41 +12,71 @@ foreach ($users as $user) {
     echo "{$user['id']} {$user['login']} {$user['password']} <hr />";
 }
 
-// проверка пустоты полей password и login
-if (!empty($_REQUEST['password']) and !empty($_REQUEST['login'])) {
-    
-    // отсечение лишнего у login
-    $login = trim($_REQUEST['login']);
-
-    // хеширование пароля
-    $password = password_hash(trim($_REQUEST['password']), PASSWORD_DEFAULT);
-
-    // запись введенных данных
-    $data = [
-        'login' => $login,
-        'password' => $password
-    ];
-
-    // выборка логина
-    $stmt = $pdo->prepare("SELECT * FROM `users` WHERE `login` = ?");
-    $stmt->execute(array($login));
-    $checkLogin = $stmt->fetch(PDO::FETCH_ASSOC);
-
-    // проверка на существования логина в бд
-    if (empty($checkLogin)) {
-        echo 'not exists';
-        $sql = "INSERT INTO users (login, password) VALUES (:login, :password)";
-        $stmt = $pdo->prepare($sql)->execute($data);
-    } else {
-        echo 'exists';
-    }
-
-}
-
 ?>
 
-<form action='index.php' method='POST'>
+<?php if (!isset($_SESSION['user'])): ?>
+
+<!-- <form action='index.php' method='POST'>
     <input name='login'>
     <input name='password' type='password'>
     <input type='submit' value='Отправить'>
-</form>
+</form> -->
+
+<style type="text/css">
+.tabs { width: 100%; padding: 0px; margin: 0 auto; }
+.tabs>input { display: none; }
+.tabs>div {
+    display: none;
+    padding: 12px;
+    border: 1px solid #C0C0C0;
+    background: #FFFFFF;
+}
+.tabs>label {
+    display: inline-block;
+    padding: 7px;
+    margin: 0 -5px -1px 0;
+    text-align: center;
+    color: #666666;
+    border: 1px solid #C0C0C0;
+    background: #E0E0E0;
+    cursor: pointer;
+}
+.tabs>input:checked + label {
+    color: #000000;
+    border: 1px solid #C0C0C0;
+    border-bottom: 1px solid #FFFFFF;
+    background: #FFFFFF;
+}
+#tab_register:checked ~ #register,
+#tab_login:checked ~ #login { display: block; }
+</style>
+
+<div class="tabs">
+    <input type="radio" name="inset" value="" id="tab_register" checked>
+    <label for="tab_register">register</label>
+
+    <input type="radio" name="inset" value="" id="tab_login">
+    <label for="tab_login">login</label>
+
+    <div id="register">
+        <form action='login.php' method='POST'>
+            <input name='login'>
+            <input name='password' type='password'>
+            <input type='submit' value='Отправить'>
+        </form>
+    </div>
+    <div id="login">
+    <form action='index.php' method='POST'>
+        <input name='login'>
+        <input name='password' type='password'>
+        <input type='submit' value='Отправить'>
+    </form>
+    </div>
+</div>
+
+<?php else: ?>
+
+<p>Wanna <a href="logout.php">exit</a>?</p>
+<p>you session is <?php $_SESSION['user']; ?></p>
+
+<?php endif; ?>
